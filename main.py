1cc56328393f0fe94b6a5f8ee3e887045c17bf6c
uuparupa <uparupa@uuparupa-no-MacBook-Air.local>
2019-11-10 20:40:26 +0900

    終わり

--- main.py
+++ main.py
@@ -28,12 +28,15 @@ def ma():
     furl = UR + "/1/"
     #ここまで
     #1ページ目の内容取得
-    rf = requests.get(furl)
+    try:
+        rf = requests.get(furl)
+    except:
+        tkmsg.showinfo('info',"何らかのエラーが発生しました。正しいURLを入力しているかご確認ください。")
     soup = BeautifulSoup(rf.content, "html.parser")
     resF = soup.find_all("p")
     #ここまで１ページ目の内容取得
     #info表示
-    tkmsg.showinfo('info',"処理を開始しました。結果はresult.txtに書き込まれます。5分ほどお待ちください。(現在バグ)")
+    tkmsg.showinfo('info',"OKを押すと処理を開始します。")
     #ココマデinfo
     print(resF)
     run(UR,resF)
@@ -42,6 +45,7 @@ def ma():
 def run(UR,resF):
     i = 0
     sd = ch(i,"",resF)
+    tkmsg.showinfo('info',"処理を開始しました。このポップアップは処理終了後自動的に消滅します。")
     while not sd == "y":
         i += 1
         print(i)
@@ -58,7 +62,7 @@ def run(UR,resF):
         f.close()
         print(res)
         sleep(5)
-        
+    tkmsg.showinfo('info',"処理を終了しました。結果はresult.txtに出力されています。ウィンドウを閉じることができます。")
 def ch(i,res,resF):
     if not i == 1:
         if res == resF or res == "<p>The article you were looking for was not found, but maybe try looking again!</p>" :
@@ -71,13 +75,13 @@ def ch(i,res,resF):
     return sd
 #tkinter setup
 ro = tk.Tk()
-ro.title(" 記事文字起こし") 
+ro.title("記事文字起こし") 
 ro.geometry("640x480")
 lbl = tk.Label(text='urlを入力してください。')
 lbl.place(x=100, y=70)
 txt = tk.Entry(width=20)
 txt.place(x=100, y=100)
-btn = tk.Button(ro, text='送信', command=ma)
-btn.place(x=100, y=200 )
+btn = tk.Button(ro, text='送信', command=ma,bg='#4076e3')
+btn.place(x=100, y=200)
 ro.mainloop()
 #ここまで
